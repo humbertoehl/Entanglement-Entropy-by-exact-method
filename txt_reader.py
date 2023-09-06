7f806b4976464af0c9605db0ee634d00027ad0db
humbertoehl <humberto.ehl@outlook.com>
2023-09-06 16:58:45 -0600

    Add input interaction on txt_reader

--- txt_reader.py
+++ txt_reader.py
@@ -25,7 +25,8 @@ def plot_txt_data(file_path):
     plt.semilogx(ratio, f, label='F')
     plt.semilogx(ratio, s_ent, label='S_Ent')
     plt.semilogx(ratio, s_ent2, label='S_Ent2')
-    plt.semilogx(ratio, o_dw, label='O_DW')
+    if CAVITY==True:
+        plt.semilogx(ratio, o_dw, label='O_DW')
 
     # Set labels and title
     plt.xlabel('Ratio (log scale)')
@@ -37,8 +38,35 @@ def plot_txt_data(file_path):
 
     # Show the plot
     plt.grid()
-    #plt.show()
-    plt.savefig("plt_from_txt")
+    plt.show()
+    #plt.savefig("plt_from_txt")
+
+while True:
+    
+    while True:
+        N = input("Input number of particles N [4/6/8]: ")
+        if N in ["4", "6", "8"]:
+            N = int(N)
+            break
+
+    while True:
+        M = input("Input number of lattice sites M [4/6/8]: ")
+        if M in ["4", "6", "8"]:
+            M = int(M)
+            break
+
+    while True:
+        extended_model = input("Extended Bose-Hubbard (include cavity part)?  [y/n]: ").lower()
+        if extended_model == "y":
+            CAVITY = True
+            break
+        elif extended_model == "n":
+            CAVITY = False
+            break
 
 
-plot_txt_data("results(N=4,M=4,Cav=False).txt")
+    try:
+        plot_txt_data("txt_results/results(N={},M={},Cav={}).txt".format(N,M,CAVITY))
+        break
+    except FileNotFoundError:
+        print("File non-existent, try to create it using \"Perform_calculation.py\"")
